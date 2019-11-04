e5fcc952752f2e1cd92b766d65662767814fb10d
karthikeyan <karthikvc@gmail.com>
2019-11-04 18:09:02 +0530

    ui changes

--- YuluDemoTests/YuluDemoTests.swift
+++ YuluDemoTests/YuluDemoTests.swift
@@ -93,11 +93,6 @@ class YuluDemoTests: XCTestCase {
         wait(for: [newplaceexpectation], timeout: 10)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
